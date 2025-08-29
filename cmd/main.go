d974bfef004281e66d3eef6505a23ab2501610eb
rupesh-hub <mahanta.rupesh704@gmail.com>
2025-08-29 11:27:47 +0530

    Mysql config file error resolution

--- cmd/main.go
+++ cmd/main.go
@@ -5,23 +5,25 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/go-sql-driver/mysql"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/rupeshmahanta/socialmedia-go/cmd/api"
 	"github.com/rupeshmahanta/socialmedia-go/configs"
+	"github.com/rupeshmahanta/socialmedia-go/db"
 )
 
 func main() {
-	// cfg := mysql.Config{
-	// 	User:                 configs.Envs.DBUser,
-	// 	Passwd:               configs.Envs.DBPassword,
-	// 	Addr:                 configs.Envs.DBAddress,
-	// 	DBName:               configs.Envs.DBName,
-	// 	Net:                  "tcp",
-	// 	AllowNativePasswords: true,
-	// 	ParseTime:            true,
-	// }
-	// db, err := db.NewMySQLStorage(cfg)
-	db, err := sql.Open("mysql", "root:9437!Nrupendra@tcp(127.0.0.1:3306)/test")
+	cfg := mysql.Config{
+		User:                 configs.Envs.DBUser,
+		Passwd:               configs.Envs.DBPassword,
+		Addr:                 configs.Envs.DBAddress,
+		DBName:               configs.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	}
+	db, err := db.NewMySQLStorage(cfg)
+	// db, err := sql.Open("mysql", "root:9437!Nrupendra@tcp(127.0.0.1:3306)/test")
 	if err != nil {
 		log.Fatal("Error in connecting to database", err)
 	}
